b0e325ce4e421e2ee036180fc21eb1189122ca7b
tangguofeng2000 <150072372+tangguofeng2000@users.noreply.github.com>
2023-11-07 00:47:32 -0500

    Update initDatabase.py
    关于dbInfo.py的连锁更新

--- backend/initDatabase.py
+++ backend/initDatabase.py
@@ -1,13 +1,13 @@
 # 文档注释
-# 1. 在运行该文档前，请在mySQL中创建一个名为finalProject的database
-# 2. 在运行该文档前，请将'mysql://root:YourPassword@localhost:3306/finalProject'中的‘YourPassword’改为您mySQL数据库的登录密码
+# 在运行该文档前，请将在db_Info文档中填写您关于database的相关信息
 
 from flask import Flask
 from flask_sqlalchemy import  SQLAlchemy
 import pymysql
+import dbInfo
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:YourPassword@localhost:3306/finalProject'
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://" + dbInfo.user_name + ":" + dbInfo.password + "@" + dbInfo.host + "/" + dbInfo.db_name
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -17,6 +17,8 @@ class User(db.Model):
     # 定义表字段
     user_id = db.Column(db.String(48), primary_key=True)
     password = db.Column(db.String(48))
+
+
 
 def init_db():
     db.drop_all()
