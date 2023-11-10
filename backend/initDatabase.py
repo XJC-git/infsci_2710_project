5fbec522817fee94c22cf832ed0ea5ad40f343bd
tangguofeng2000 <150072372+tangguofeng2000@users.noreply.github.com>
2023-11-10 17:35:34 -0500

    Update initDatabase.py
    将构建完毕的数据表建成，完整数据表的参数和定义过程都在该文档内（后续除非特殊情况，否则不再改动此文档）

--- backend/initDatabase.py
+++ backend/initDatabase.py
@@ -11,13 +11,115 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://" + dbInfo.user_name + ":" + db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class Users(db.Model):
     # 定义表名
     __tablename__ = 'user'
     # 定义表字段
-    user_id = db.Column(db.String(48), primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(48))
     password = db.Column(db.String(48))
+    user_type = db.Column(db.String(48))
 
+
+class Customers(db.Model):
+    # 定义表名
+    __tablename__ = 'customers'
+    # 定义表字段
+    customer_id = db.Column(db.Integer, db.ForeignKey("user.user_id"), primary_key=True)
+    address = db.Column(db.String(48))
+    state = db.Column(db.String(48))
+    city = db.Column(db.String(48))
+    zip_code = db.Column(db.Integer)
+    kind = db.Column(db.String(12))
+
+
+class Region(db.Model):
+    # 定义表名
+    __tablename__ = 'region'
+    # 定义表字段
+    region_id = db.Column(db.Integer, primary_key=True)
+    region_name = db.Column(db.String(48))
+    region_manager = db.Column(db.String(48))
+
+class Store(db.Model):
+    # 定义表名
+    __tablename__ = 'store'
+    # 定义表字段
+    store_id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(48))
+    state = db.Column(db.String(48))
+    city = db.Column(db.String(48))
+    manager = db.Column(db.String(48))
+    number_of_salesperson = db.Column(db.Integer)
+    region = db.Column(db.Integer, db.ForeignKey("region.region_id"))
+
+
+class Salespersons(db.Model):
+    # 定义表名
+    __tablename__ = 'salespersons'
+    # 定义表字段
+    salesperson_id = db.Column(db.Integer, db.ForeignKey("user.user_id"), primary_key=True)
+    name = db.Column(db.String(48))
+    email = db.Column(db.String(48))
+    job_title = db.Column(db.String(48))
+    store_assigned = db.Column(db.Integer, db.ForeignKey("store.store_id"))
+    salary = db.Column(db.Float)
+    state = db.Column(db.String(48))
+    city = db.Column(db.String(48))
+    address = db.Column(db.String(48))
+    zip_code = db.Column(db.Integer)
+
+
+class Products(db.Model):
+    # 定义表名
+    __tablename__ = 'products'
+    # 定义表字段
+    product_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(48))
+    category = db.Column(db.String(48))
+    price = db.Column(db.Float)
+    inventory_amount = db.Column(db.Integer)
+
+
+class Transactions(db.Model):
+    # 定义表名
+    __tablename__ = 'transactions'
+    # 定义表字段
+    transaction_id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    salesperson_id = db.Column(db.Integer, db.ForeignKey("salespersons.salesperson_id"))
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.customer_id"))
+
+
+class Sub_Transactions(db.Model):
+    # 定义表名
+    __tablename__ = 'sub_transactions'
+    # 定义表字段
+    sub_transaction_id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey("products.product_id"))
+    transaction_id = db.Column(db.Integer, db.ForeignKey("transactions.transaction_id"))
+    quantity = db.Column(db.Integer)
+
+
+
+class Home_Customers_Details(db.Model):
+    # 定义表名
+    __tablename__ = 'home_customers'
+    # 定义表字段
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.customer_id"), primary_key=True)
+    marriage_status = db.Column(db.Boolean)
+    gender = db.Column(db.String(24))
+    age = db.Column(db.Integer)
+
+
+class Business_Customer_Details(db.Model):
+    # 定义表名
+    __tablename__ = 'business_customers'
+    # 定义表字段
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.customer_id"), primary_key=True)
+    company_name = db.Column(db.String(48))
+    business_category = db.Column(db.String(48))
+    company_income = db.Column(db.Float)
 
 
 def init_db():
